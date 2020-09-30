4dbbf7b0f34abea3395882ffb0e0b84ac5880ca9
Maiko Tan <maiko.tan.coding@gmail.com>
2020-09-30 11:35:43 +0000

    feat: add interface declation

---
+++ src/utils/ACTListener.d.ts
@@ -0,0 +1,24 @@
+interface Party {
+  id: string;
+  name: string;
+  worldId: number;
+  job: number;
+  inParty: boolean;
+}
+
+interface Events {
+  "CombatData": () => void;
+  "LogLine": (ev: { line: string[], rawLine: string }) => void;
+  "ChangeZone": (ev: { zoneID: string }) => void;
+  "ChangePrimaryPlayer": (ev: { charID: string, charName: string }) => void;
+  "OnlineStatusChanged": (ev: { target: string, rawStatus: number, status: string}) => void;
+  "PartyChanged": (ev: { party: Party[] }) => void;
+}
+
+declare function addOverlayListener<E extends keyof Events>(
+    event: E, listener: Events[E]
+  ): void;
+
+export {
+  addOverlayListener,
+};
