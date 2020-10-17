630c69216a073ff6c6cc322ffd78fc87fca67e26
Maiko Tan <maiko.tan.coding@gmail.com>
2020-10-17 22:55:39 +0800

    feat: add TimedActionIcon

--- src/components/common/Icon.tsx
+++ src/components/common/Icon.tsx
@@ -1,4 +1,6 @@
-import React from "react";
+import React, { useEffect, useRef, useState } from "react";
+
+import "./Icon.css";
 
 interface ApiDataState {
   Icon: string;
@@ -7,16 +9,18 @@ interface ApiDataState {
 
 interface IconProps {
   apiDataUrl: string;
+  text?: string;
+  invalid?: boolean;
 }
 
 const Icon: React.FC<IconProps> = (props: IconProps) => {
-  const { apiDataUrl } = props;
-  const [ apiData, setApiData ] = React.useState<ApiDataState>();
+  const { apiDataUrl, text, invalid } = props;
+  const [apiData, setApiData] = React.useState<ApiDataState>();
 
   React.useEffect(() => {
     let current = true;
     void (async () => {
-      const data = await (await fetch(`https://xivapi.com/${apiDataUrl}`, {
+      const data = await (await fetch(`https://xivapi.com/${apiDataUrl}?columns=ID,Name,Icon`, {
         mode: "cors"
       })).json();
       if (current) {
@@ -27,22 +31,69 @@ const Icon: React.FC<IconProps> = (props: IconProps) => {
     return () => {
       current = false;
     };
-  }, [apiDataUrl]);
+  }, [apiDataUrl, text]);
 
   if (!apiData || !apiData.Icon) {
     return null;
   }
 
+  const classnames = [
+    "icon",
+    invalid ? "invalid": "",
+  ];
+
   return (
     <>
-      <div>
+      <div className={classnames.join(" ")}>
         <img
           src={`https://xivapi.com/${apiData.Icon}`}
           alt={apiData.Name}
         />
+        <span className="text">{text ? text : ""}</span>
       </div>
     </>
   );
 };
 
-export default Icon;
+interface ActionIconProps {
+  actionId: number;
+  cooldown: number;
+}
+
+const TimedActionIcon: React.FC<ActionIconProps> = (props: ActionIconProps) => {
+
+  const { actionId, cooldown } = props;
+
+  const endTime = useRef<number>(Date.now() + (cooldown || 0) * 1000);
+  const [cd, setCd] = useState(Number(cooldown));
+
+  useEffect(() => {
+    if (cooldown && cd >= 0) {
+      const timer = setTimeout(() => {
+        setCd((endTime.current - Date.now()) / 1000);
+      }, 30);
+
+      return () => {
+        if (timer) {
+          clearTimeout(timer);
+        }
+      };
+    }
+  }, [cd, cooldown]);
+
+  return (
+    <>
+      <Icon
+        apiDataUrl={`Action/${actionId}`}
+        text={`${(cooldown && cd > 0) ? Math.ceil(cd).toFixed() : ""}`}
+        invalid={cd > 0}
+      />
+    </>
+  );
+};
+
+
+export {
+  Icon,
+  TimedActionIcon,
+};
