de3b5a56f27380f1b517bff517784ba1ecedd173
Maiko Tan <maiko.tan.coding@gmail.com>
2021-01-08 10:16:18 +0800

    refactor: enum factory

--- src/model/loglines/logfactory.ts
+++ src/model/loglines/logfactory.ts
@@ -8,32 +8,44 @@ import LogNetworkBuff from "./log_network_buff";
 import LogNetworkBuffRemove from "./log_network_buff_remove";
 import LogNetworkStatusEffects from "./log_network_status_effects";
 
+enum LogType {
+  ChangeZone = 0x01,
+  PlayerStats = 0x0C,
+  NetworkStartsCasting = 0x14,
+  NetworkAbility = 0x15,
+  NetworkAoeAbility = 0x16,
+  NetworkDeath = 0x19,
+  NetworkBuff = 0x1A,
+  NetworkBuffRemove = 0x1E,
+  NetworkStatusEffects = 0x26,
+}
+
 class LogFactory {
   getLog(loglines: string[]): Base {
     switch (parseInt(loglines[0])) {
-    case 0x01:
+    case LogType.ChangeZone:
       return new LogChangeZone(loglines);
 
-    case 0x0C:
+    case LogType.PlayerStats:
       return new LogPlayerStats(loglines);
 
-    case 0x14:
+    case LogType.NetworkStartsCasting:
       return new LogNetworkStartsCasting(loglines);
 
-    case 0x15:
-    case 0x16:
+    case LogType.NetworkAbility:
+    case LogType.NetworkAoeAbility:
       return new LogNetworkAbility(loglines);
 
-    case 0x19:
+    case LogType.NetworkDeath:
       return new LogNetworkDeath(loglines);
 
-    case 0x1A:
+    case LogType.NetworkBuff:
       return new LogNetworkBuff(loglines);
 
-    case 0x1E:
+    case LogType.NetworkBuffRemove:
       return new LogNetworkBuffRemove(loglines);
 
-    case 0x26:
+    case LogType.NetworkStatusEffects:
       return new LogNetworkStatusEffects(loglines);
 
     default:
