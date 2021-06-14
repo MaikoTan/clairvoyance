0cb705f13c07dbdc8884310eae44b73f44f925fe
迷子 (Maiko Tan) <maiko.tan.coding@gmail.com>
2021-06-14 14:09:19 +0000

    feat: add level in player

--- src/model/player.ts
+++ src/model/player.ts
@@ -1,15 +1,18 @@
 interface PlayerBase {
   id: string;
   name: string;
+  level: number;
 }
 
 class Player implements PlayerBase {
   id: string;
   name: string;
+  level: number;
 
   constructor(player: PlayerBase) {
     this.id = player.id;
     this.name = player.name;
+    this.level = player.level;
   }
 }
 
